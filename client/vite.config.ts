76d9844f7815006cbbee3513fb466e71dbccf958
sid978123 <siddharthaman978@gmail.com>
2026-09-21 02:52:21 +0530

    change  name

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
+      "/api": {
+        target: "https://llosers-api.onrender.com",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
